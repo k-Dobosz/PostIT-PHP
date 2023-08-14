20d7b3f481011aa60052e012cb6daea2843c6dfe
k-Dobosz <kdobosz02@gmail.com>
2023-08-14 02:08:42 +0200

    Add initial user domain and initial doctrine configuration

---
+++ src/User/Controller/UserLoginController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\User\Controller;
+
+use App\User\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
+
+class UserLoginController extends AbstractController
+{
+    #[Route('/api/login', name: 'app_api_login')]
+    public function index(#[CurrentUser] ?User $user): Response
+    {
+        if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return $this->json([
+            'user' => $user->getUserIdentifier(),
+        ]);
+    }
+}
